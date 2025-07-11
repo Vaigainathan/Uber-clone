18f0b309d0a66216333979231b2b158147e8ce42
Vaigainathan <itsvaigai1211@gmail.com>
2025-07-11 10:26:35 +0530

    Ride Update

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,5 +1,4 @@
-import { useUser } from "@clerk/clerk-expo";
-import { useAuth } from "@clerk/clerk-expo";
+import { useUser, useAuth } from "@clerk/clerk-expo";
 import * as Location from "expo-location";
 import { router } from "expo-router";
 import { useState, useEffect } from "react";
